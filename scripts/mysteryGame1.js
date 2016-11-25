d1a2619beb0dc50941df9d054200a364cde706e2
Nicholas H <syncopika@users.noreply.github.com>
2016-11-25 11:40:20 -0500

    Update mysteryGame1.js

--- scripts/mysteryGame1.js
+++ scripts/mysteryGame1.js
@@ -5,12 +5,12 @@ Screen.setDialogBox("artAssets/dialogBar.png");
 Routes["mainRoute"] = [
 	SetScene.newBGwithMusic("artAssets/readingroom1.png", "audioAssets/112416melancholypianobgm.wav"),
 	SetScene.dialog("", "'Twas one night whilst in my reading room, analysing lines from Ovid, as I often do,"),
-	SetScene.dialog("", "when I was startled by several rather large knocks upon my front door."),
+	SetScene.dialogWithEffect("", "when I was startled by several rather large knocks upon my front door.", "audioAssets/knockingSoundsSmall.wav"),
 	SetScene.dialog("", "mind you, it was a quarter before 9 o'clock and I was not to be expecting guests."),
-	SetScene.dialog("", "'Perhaps it is the constable?,' I thought, for a string of petty crimes had been committed lately."),
-	SetScene.dialog("", "Anyhow, I hurried to the door."),
+	SetScene.dialogWithEffect("", "'Perhaps it is the constable?,' I thought, for a string of petty crimes had been committed lately.", "audioAssets/knockingSoundsSmall.wav"),
+	SetScene.dialogWithEffect("", "Anyhow, I hurried to the door.", "audioAssets/knockingSounds.wav"),
 	SetScene.background("artAssets/stairs.png"),
-	SetScene.dialog("", "The knocking persisted as I went down the stairs, and as I got to the door."),
+	SetScene.dialogWithEffect("", "The knocking persisted as I went down the stairs, and as I got to the door.", "audioAssets/knockingSounds.wav"),
 	SetScene.background("artAssets/neighborhood.png"),
 	SetScene.dialog("", "I opened the door, but there was no one. There was not a soul to be seen at all in the street!"),
 	SetScene.dialog("", "What a curious phenomenon. As I was about to close the door, I saw on my doorstep..."),
@@ -33,5 +33,6 @@ Routes["mainRoute"] = [
 	SetScene.newBGwithMusic("artAssets/upstairs.png", "audioAssets/112416melancholypianobgm2.wav"),
 	SetScene.dialog("", "On the night of the twelfth, I realized I had made a terrible mistake; a mistake that would continue to haunt me forever."),
 	SetScene.background("artAssets/lame.png"),
-	SetScene.dialog("", "gahhhh, this story sounds so lame!!")
+	SetScene.dialog("", "gahhhh, this story sounds so lame!!"),
+	SetScene.background("thankyou.png")
 ];
